06c8cd5587836035799ea45c29b0a1c5fc9c39b1
Patrick Sinclair <patricksinclair@hotmail.co.uk>
2019-01-08 13:32:54 +0000

    second commit

--- src/BioSystem.java
+++ src/BioSystem.java
@@ -1,3 +1,5 @@
+import com.sun.xml.internal.org.jvnet.mimepull.MIMEConfig;
+
 import java.util.Random;
 
 public class BioSystem {
@@ -5,12 +7,174 @@ public class BioSystem {
     Random rand = new Random();
 
     private int L, K;
-    private double alpha;
+    private double alpha, c_max;
     Microhabitat[] microhabitats;
     private int initialMaxRandPop = 500; // I'll fill the first few microhabitats with a random number (0-maxrandpop) of random bacteria
-    private int initialPopZone
+    private int initialPopZone = 200;
+    private double timeElapsed;
 
 
+    public BioSystem(int L, int K, double alpha, double c_max){
+        this.L = L;
+        this.K = K;
+        this.alpha = alpha;
+        this.timeElapsed = 0.;
+
+        this.microhabitats = new Microhabitat[L];
+
+        for(int i = 0; i < L; i++) {
+            microhabitats[i] = new Microhabitat(K, BioSystem.getCValWithOffset(i, c_max, alpha, L));
+        }
+
+        microhabitats[L-1].setSurface(true);
+        microhabitats[L-1].setBiofilm_region(true);
+
+    }
+
+    public double getTimeElapsed(){return timeElapsed;}
+
+
+    public int getTotalN(){
+        int runningTotal = 0;
+
+        for(Microhabitat m : microhabitats){
+            runningTotal += m.getN();
+        }
+        return  runningTotal;
+    }
+
+
+    public boolean everythingIsDead(){
+        //returns true if the population of the system is 0
+
+        return getTotalN() == 0;
+    }
+
+    public int getBiofilmEdge(){
+        //finds the microhabitat furthest from the surface which is part of the biofilm
+        int edgeIndex = 0;
+        for(int i = 0; i < L; i++){
+            if(microhabitats[i].getBiofilm_region()){
+                edgeIndex = i;
+                break;
+            }
+        }
+        return edgeIndex;
+    }
+
+    public int[] getPopulationDistribution(){
+        int[] popSizes = new int[L];
+
+        for(int i = 0; i < L; i++){
+            popSizes[i] = microhabitats[i].getN();
+        }
+        return popSizes;
+    }
+
+    public double[] getAvgGenotypeDistribution(){
+        double[] avgGenos = new double[L];
+        for(int i = 0; i < L; i++){
+            avgGenos[i] = microhabitats[i].getAvgGenotype();
+        }
+        return avgGenos;
+    }
+
+
+
+    public int[] getRandIndexes(int randBacteriaNumber){
+        int totalCounter = 0;
+        int microhab_index = 0;
+        int bacteria_index = 0;
+
+        forloop:
+        for(int i = 0; i < L; i++){
+            if(totalCounter + microhabitats[i].getN() <= randBacteriaNumber){
+                totalCounter += microhabitats[i].getN();
+                continue forloop;
+
+            }else{
+                microhab_index = i;
+                bacteria_index = randBacteriaNumber - totalCounter;
+                break forloop;
+            }
+        }
+        return new int[]{microhab_index, bacteria_index};
+    }
+
+
+    public void migrate(int mh_index, int bac_index){
+        //Here this should only move bacteria within the biofilm region
+        //takes the arguments of the microhabitat and bacteria indexes, so it performs
+        //the necessary action on the desired one
+        int biof_edge = getBiofilmEdge();
+        double rand_bac = microhabitats[mh_index].getPopulation().get(bac_index);
+
+        if(mh_index == L-1){
+            microhabitats[mh_index].removeABacterium(bac_index);
+            microhabitats[mh_index-1].addABacterium(rand_bac);
+
+        }else if(mh_index == biof_edge){
+            //if the bacteria is at the edge of the biofilm, there's a chance it detached, depending on the stickiness
+            //ADD IN LATER
+            microhabitats[mh_index].removeABacterium(bac_index);
+            microhabitats[mh_index+1].addABacterium(rand_bac);
+
+        }else{
+            if(rand.nextBoolean()){
+                microhabitats[mh_index].removeABacterium(bac_index);
+                microhabitats[mh_index+1].addABacterium(rand_bac);
+            }else{
+                microhabitats[mh_index].removeABacterium(bac_index);
+                microhabitats[mh_index-1].addABacterium(rand_bac);
+            }
+        }
+    }
+
+
+    public void replicate(int mh_index, int bac_index){
+        microhabitats[mh_index].replicateABacterium(bac_index);
+    }
+
+    public void die(int mh_index, int bac_index){
+        microhabitats[mh_index].removeABacterium(bac_index);
+    }
+
+
+    public void performAction(){
+
+        int N = getTotalN(); //no. of bacteria in the system
+        double R_max = 5.2;
+
+        if(N==0){
+            microhabitats[L-1].addARandomBacterium();
+        }else{
+
+            int randBacIndex = rand.nextInt(getTotalN());
+            int [] randIndexes = getRandIndexes(randBacIndex);
+            int microhabIndex = randIndexes[0], bacteriaIndex = randIndexes[1];
+            Microhabitat rand_mh = microhabitats[microhabIndex];
+
+            double replication_rate = rand_mh.replicationRate(bacteriaIndex);
+            double migrate_rate = rand_mh.migrate_rate();
+
+            //if statement to handle if replication or death occurs, as -ve signs need to be handled
+            if(replication_rate >= 0.){
+                double rand_chance = rand.nextDouble()*R_max;
+
+                if(rand_chance <= migrate_rate) migrate(microhabIndex, bacteriaIndex);
+                else if(rand_chance > migrate_rate && rand_chance <= replication_rate+migrate_rate) replicate(microhabIndex, bacteriaIndex);
+
+            }else{
+                double rand_chance = rand.nextDouble();
+                replication_rate*=-1.;
+
+                if(rand_chance <= migrate_rate) migrate(microhabIndex, bacteriaIndex);
+                else if(rand_chance > migrate_rate && rand_chance <= replication_rate+migrate_rate) die(microhabIndex, bacteriaIndex);
+            }
+        }
+
+        timeElapsed += 1./(double)N*R_max;
+    }
 
 
 
--- src/Microhabitat.java
+++ src/Microhabitat.java
@@ -12,12 +12,19 @@ public class Microhabitat {
     Random rand = new Random();
     private int K; //karrying kapacity
     private double c; //concentration of antimicrobial
+    private double b; //migration rate for bacteria in this microhabitat
     ArrayList<Double> population; //contains a list of doubles representing the MICs of the bacteria present
+
+    private boolean surface = false; //boolean detailing whether this is the microhabitat at the surface of the ship or not (maybe surface of biofilm too)
+    private boolean biofilm_region = false; //boolean detailing whther this microhabitat is the well-mixed ocean environment or the biofilm region
+    private double threshold_stickiness = 0.15; //fraction occupied required for biofilm formation to begin
+
 
 
     public Microhabitat(int K, double c){
         this.K = K;
         this.c = c;
+        this.b = 0.1;
         this.population = new ArrayList<>(K);
     }
 
@@ -25,8 +32,32 @@ public class Microhabitat {
     public int getN(){return population.size();}
     public double getC(){return c;}
 
+    public ArrayList<Double> getPopulation(){return population;}
+
+    public void setSurface(boolean surface){this.surface = surface;}
+    public boolean getBiofilm_region(){return this.biofilm_region;}
+    public void setBiofilm_region(boolean biofilm_region){this.biofilm_region = biofilm_region;}
+
     public double fractionFull(){
         return (double)getN()/(double)K;
+    }
+
+
+    public double stickiness(){
+        //this returns a paramteter between 0 and 1 which gives the stickiness of the biofilm
+        //the closer to 1, the stickier the biofilm, the less easy it is for the bacteria to move
+        if(surface) return 1.;
+        else if(getN() < threshold_stickiness*K) return 0.0;
+
+        else{
+            double alpha = Math.log(2.)/((1. - threshold_stickiness)*K);
+            return Math.min(Math.exp(alpha*(getN() - threshold_stickiness*K)) - 1., 1.);
+        }
+    }
+
+
+    public double migrate_rate(){
+        return b*(1. - stickiness());
     }
 
 
@@ -35,23 +66,42 @@ public class Microhabitat {
     }
 
     public double phi_c(int index){
-        double phi_c = 1. - (c/beta(index))*(c/beta(index));
-        return  (phi_c > 0.) ? phi_c : 0.;
+        //double phi_c = 1. - (c/beta(index))*(c/beta(index));
+        //return  (phi_c > 0.) ? phi_c : 0.;
+        //use the 6x/1+5x one for this at the mo
+        double cB = c/beta(index);
+        return 1. - 6*cB*cB/(5. + cB*cB);
     }
 
     public double replicationRate(int index){
         double gRate = phi_c(index)*(1. - getN()/K);
-        return  (gRate > 0.) ? gRate : 0.;
+        return  (gRate > 0.) ? gRate : phi_c(index);
     }
 
 
 
+    public double getAvgGenotype(){
+        if(getN() == 0) return 0.;
+        else{
+            double sum = 0.;
+            for(Double geno : population){
+                sum += geno;
+            }
+            return sum/(double)getN();
+        }
+    }
 
 
 
     public void randomlyPopulate(){
 
         for(int i = 0; i < K; i++){
+            population.add(MIC_distribution.sample());
+        }
+    }
+
+    public void randomlyPopulate(int randPopSize){
+        for(int i = 0; i < randPopSize; i++){
             population.add(MIC_distribution.sample());
         }
     }
@@ -64,6 +114,8 @@ public class Microhabitat {
     public void replicateABacterium(int index){
         population.add(population.get(index));
     }
+
+    public void addABacterium(double MIC){population.add(MIC);}
 
     public void removeABacterium(int index){
         population.remove(index);
